11b7ca078ff9086f4b2932b13d1299955d63a683
ashikalahi546 <144088399+ashikalahi546@users.noreply.github.com>
2024-07-29 18:05:30 +0600

    update

--- src/components/menu/Menu.js
+++ src/components/menu/Menu.js
@@ -14,26 +14,28 @@ const Menu = () => {
           className="cursor-pointer pl-4"
         >
           <div className="flex items-center justify-between group relative ease-in-out duration-300 hover:text-primary">
-      <span className="group-hover:text-primary">
-        {menu?.title}
-      </span>
-      {menu?.submenu && (
-        <MdOutlineChevronRight
-          className={` mr-2 z-20  text-livebg ${menu?.submenu ? "transition-transform duration-300 ease-in-out group-hover:rotate-180 group-hover:text-primary" : ""} `}
-        />
-      )}
-    </div>
+            <span className="group-hover:text-primary">{menu?.title}</span>
+            {menu?.submenu && (
+              <MdOutlineChevronRight
+                className={` mr-2 z-20  text-livebg ${
+                  menu?.submenu
+                    ? "transition-transform duration-300 ease-in-out group-hover:rotate-180 group-hover:text-primary"
+                    : ""
+                } `}
+              />
+            )}
+          </div>
           {hover === index && menu?.submenu && (
             <ul className="bg-white absolute left-[100%] h-[355px] top-0 w-[230px] flex flex-col gap-y-[10px] py-2.5 duration-300">
-           
               {menu?.submenu?.map((sub, subIndex) => (
                 <li key={subIndex}>
-                  <span className='pl-4 hover:text-primary ease-in-out transform duration-300'>{sub?.title}</span>
+                  <span className="pl-4 hover:text-primary ease-in-out transform duration-300">
+                    {sub?.title}
+                  </span>
                 </li>
               ))}
               <div className="h-[355px] w-[1px] bg-gray-100 absolute top-0"></div>
             </ul>
-            
           )}
         </li>
       ))}
